2c221dc9fc5ba3c396bfe5756c4384a392d590eb
sunny <sunnya2023@gmail.com>
2024-05-10 03:32:29 +0900

    4-profile update

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,159 @@
+import Notification from "../models/notification.medel.js";
+import User from "../models/user.model.js";
+import bcrypt from "bcrypt";
+import { v2 as cloudinary } from "cloudinary";
+export const getUserProfile = async (req, res) => {
+  const { username } = req.params;
+  try {
+    const user = await User.findOne({ username }).select("-password");
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.status(200).json(user);
+  } catch (error) {
+    console.log("Error in getUserProfile", error.message);
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const followUnfollowUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const userToModify = await User.findById(id);
+    const currentUser = await User.findById(req.user._id);
+    if (id === req.user._id.toString()) {
+      return res
+        .status(400)
+        .json({ error: "You can't follow/unfollw yourself" });
+    }
+
+    if (!userToModify || !currentUser) {
+      return res.status(400).json({ error: "User not found" });
+    }
+
+    //follow 기능
+    const isFollowing = currentUser.following.includes(id);
+    if (isFollowing) {
+      //Unfollow the user
+      await User.findByIdAndUpdate(id, { $pull: { followers: req.user._id } });
+      await User.findByIdAndUpdate(req.user._id, { $pull: { following: id } });
+      //return the id of the user as a response
+
+      res.status(200).json({ message: "User unfollowed successfully" });
+    } else {
+      //follow the user
+      await User.findByIdAndUpdate(id, { $push: { followers: req.user._id } });
+      await User.findByIdAndUpdate(req.user._id, { $push: { following: id } });
+      //send notification to the user
+      const newNotification = new Notification({
+        from: req.user._id,
+        to: userToModify._id,
+        type: "follow",
+      });
+
+      await newNotification.save();
+      //return the id of the user as a response
+
+      res.status(200).json({ message: "User followed successfully" });
+    }
+  } catch (error) {
+    console.log("Error in followUnfollowUser", error.message);
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const getSuggestedUsers = async (req, res) => {
+  try {
+    const userId = req.user._id;
+    const usersFollowedByMe = await User.findById(userId).select("following");
+    const users = await User.aggregate([
+      {
+        $match: {
+          _id: { $ne: userId },
+        },
+      },
+      { $sample: { size: 10 } },
+    ]);
+
+    const filteredUsers = users.filter(
+      (user) => !usersFollowedByMe.following.includes(userId)
+    );
+    const sugestedUsers = filteredUsers.slice(0, 4);
+    sugestedUsers.forEach((user) => (user.password = null));
+    res.status(200).json(sugestedUsers);
+  } catch (error) {
+    console.log("Error in getSuggestedUsers", error.message);
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const updateUser = async (req, res) => {
+  const { username, email, currentPassword, newPassword, bio, link } = req.body;
+  let { profileImg, coverImg } = req.body;
+  const userId = req.user._id;
+  const passwordRegex = /^(?=.*?[A-Za-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{6,}$/;
+
+  try {
+    let user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+    if (
+      (!newPassword && currentPassword) ||
+      (!currentPassword && newPassword)
+    ) {
+      return res.status(404).json({
+        message: "Please provide both current password and new password ",
+      });
+    }
+    if (currentPassword && newPassword) {
+      const isMatch = await bcrypt.compare(currentPassword, user.password);
+      if (!isMatch) {
+        return res.status(404).json({ error: "비밀번호가 일치하지 않습니다." });
+      } else if (!passwordRegex.test(newPassword)) {
+        return res.status(404).json({
+          error:
+            "비밀번호는 6자 이상이어야 하며, 숫자/소문자/특수문자를 모두 포함해야 합니다.",
+        });
+      }
+
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(newPassword, salt);
+    }
+
+    if (profileImg) {
+      if (user.profileImg) {
+        await cloudinary.uploader.destroy(
+          user.profileImg.split("/").pop().split(".")[0]
+        );
+      }
+      const uploadedResponse = await cloudinary.uploader.upload(profileImg);
+      profileImg = uploadedResponse.secure_url();
+    }
+    if (coverImg) {
+      if (user.coverImg) {
+        await cloudinary.uploader.destroy(
+          user.coverImg.split("/").pop().split(".")[0]
+        );
+      }
+      const uploadedResponse = await cloudinary.uploader.upload(coverImg);
+      coverImg = uploadedResponse.secure_url();
+    }
+
+    user.username = username || user.username;
+    user.email = email || user.email;
+    user.bio = bio || user.bio;
+    user.link = link || user.link;
+    user.profileImg = profileImg || user.profileImg;
+    user.coverImg = coverImg || user.coverImg;
+
+    user = await user.save();
+    //passwrod should be null in response
+    user.password = null;
+    return res.status(200).json(user);
+  } catch (error) {
+    console.log("Error in updateUser", error.message);
+    res.status(500).json({ error: error.message });
+  }
+};
--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,24 @@
 import express from "express";
 import dotenv from "dotenv";
-import authRoutes from "./routes/auth.route.js";
-import connectMongoDB from "./db/connectMongoDB.js";
 import cookieParser from "cookie-parser";
+import { v2 as cloudinary } from "cloudinary";
+
+import authRoutes from "./routes/auth.route.js";
+import userRoutes from "./routes/user.route.js";
+
+import connectMongoDB from "./db/connectMongoDB.js";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 dotenv.config();
-console.log(process.env.MONGO_URL);
+// console.log(process.env.MONGO_URL);
+
+cloudinary.config({
+  cloud_name: process.env.cloud_name,
+  api_key: process.env.cloud_API_KEY,
+  api_secret: process.env.cloud_API_SECRET,
+});
 
 //미들웨어
 app.use(express.json()); //to parse req.body
@@ -16,6 +26,7 @@ app.use(express.urlencoded({ extended: true })); //to parse form data(urlencoded
 app.use(cookieParser()); //쿠키 파싱 미들웨어
 
 app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
